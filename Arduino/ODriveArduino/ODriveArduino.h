41c9ff369de136f4c613bacdbb275fce6d8addf5
Clément Boesmier <cle.chec@laposte.net>
2022-01-14 21:09:39 +0100

    fix: function name, start cleanup, start document

--- Arduino/ODriveArduino/ODriveArduino.h
+++ Arduino/ODriveArduino/ODriveArduino.h
@@ -7,19 +7,41 @@
 
 class ODriveArduino {
 public:
+    /**
+     * @brief Construct a new ODriveArduino object
+     *
+     * @param[in] serial The serial port to use to communicate with the ODrive
+     */
     ODriveArduino(Stream& serial);
 
-    // Commands
-    void SetPosition(int motor_number, float position);
-    void SetPosition(int motor_number, float position, float velocity_feedforward);
-    void SetPosition(int motor_number, float position, float velocity_feedforward, float current_feedforward);
-    void SetVelocity(int motor_number, float velocity);
-    void SetVelocity(int motor_number, float velocity, float current_feedforward);
-    void SetCurrent(int motor_number, float current);
-    void TrapezoidalMove(int motor_number, float position);
-    // Getters
-    float GetVelocity(int motor_number);
-    float GetPosition(int motor_number);
+    //TODO: comprendre comment marche velocity_feedforward
+    //TODO: comprendre comment marche current_feedforward
+    /**
+     * @brief Set the Position of a motor
+     *
+     * @param[in] motor_number The motor to set the position of
+     * @param[in] position The position to set the motor to in tour number
+     * @param[in] velocity_feedforward
+     * @param[in] current_feedforward
+     */
+    void setPosition(int motor_number, float position, float velocity_feedforward = 0, 0f, float current_feedforward = 0.0f);
+
+    //TODO: comprendre comment marche current_feedforward
+    /**
+     * @brief Set the Velocity of a motor
+     *
+     * @param[in] motor_number The motor to set the velocity of
+     * @param[in] velocity The velocity to set the motor to
+     * @param[in] current_feedforward
+     */
+    void setVelocity(int motor_number, float velocity, float current_feedforward = 0.0f);
+
+
+    void setCurrent(int motor_number, float current);
+    void trapezoidalMove(int motor_number, float position);
+    // getters
+    float getVelocity(int motor_number);
+    float getPosition(int motor_number);
     // General params
     float readFloat();
     int32_t readInt();
